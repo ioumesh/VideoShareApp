5c0efb1be3bf16d9c1ea952ca3387c316a53782c
ioumesh <umeshkumar@zingbus.com>
2022-07-17 00:34:42 +0530

    feat:app structure

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
-import "./App.css";
-
+import styled from "styled-components";
+import Menu from "./components/Menu";
+import Navbar from "./components/Navbar";
+const Container = styled.div`
+  display: flex;
+`;
+const Main = styled.div`
+  flex: 7;
+`;
+const Wrapper = styled.div``;
 function App() {
-  return <div className="App">OTT</div>;
+  return (
+    <Container>
+      <Menu />
+      <Main>
+        <Navbar />
+        <Wrapper>Video Card</Wrapper>
+      </Main>
+    </Container>
+  );
 }
 
 export default App;
